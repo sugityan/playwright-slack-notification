6d522c6d9768e1db1b00fbff5686f316349d4610
sugityan <baketu427@gmail.com>
2026-03-14 18:31:16 +0900

    init from tutorial

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+export function helloWorld() {
+  const message = 'Hello World';
+  return message;
+}
+
+export function goodBye() {
+  const message = 'Goodbye';
+  return message;
+}
+
+export default {
+  helloWorld,
+  goodBye,
+}
